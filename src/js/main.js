46a01613fb318b4bbe1164f13518f1e39ca830f3
Marina Moroz <moroz2387moroz@yandex.ru>
2024-08-27 14:27:20 +1000

    add script

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,35 @@ window.addEventListener("DOMContentLoaded", () => {
   const ctx = canvas.getContext("2d");
 
   let size = 20;
+  let isPressed = false;
   let color = "black";
   let x;
   let y;
+
+  canvas.addEventListener("mousedown", (e) => {
+    isPressed = true;
+    x = e.offsetX;
+    y = e.offsetY;
+  });
+
+  canvas.addEventListener("mouseup", (e) => {
+    isPressed = false;
+    x = undefined;
+    y = undefined;
+  });
+
+  canvas.addEventListener("mousemove", (e) => {
+    if (isPressed) {
+      const x2 = e.offsetX;
+      const y2 = e.offsetY;
+
+      drawCircle(x2, y2);
+      drawLine(x, y, x2, y2);
+
+      x = x2;
+      y = y2;
+    }
+  });
 
   function drawCircle(x, y) {
     ctx.beginPath();
@@ -19,10 +45,7 @@ window.addEventListener("DOMContentLoaded", () => {
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.strokeStyle = color;
-    ctx.lineWidth = size;
+    ctx.lineWidth = size * 2;
     ctx.stroke();
   }
-
-  drawCircle(100, 200);
-  drawLine(300, 300, 200, 500);
 });
